11f032c6d463c74ea0da3109a1bef2b87f454533
sujithnpoojary01 <57342810+sujithnpoojary01@users.noreply.github.com>
2020-01-30 20:12:30 +0530

    Update bot_demo.py

--- bot_demo.py
+++ bot_demo.py
@@ -1,70 +1,37 @@
-from itty import *
-import urllib2
+import requests
 import json
+from flask import Flask, request
 
-def sendSparkGET(url):
-    """
-    This method is used for:
-        -retrieving message text, when the webhook is triggered with a message
-        -Getting the username of the person who posted the message if a command is recognized
-    """
-    request = urllib2.Request(url,
-                            headers={"Accept" : "application/json",
-                                     "Content-Type":"application/json"})
-    request.add_header("Authorization", "Bearer "+bearer)
-    contents = urllib2.urlopen(request).read()
-    return contents
-    
-def sendSparkPOST(url, data):
-    """
-    This method is used for:
-        -posting a message to the Spark room to confirm that a command was received and processed
-    """
-    request = urllib2.Request(url, json.dumps(data),
-                            headers={"Accept" : "application/json",
-                                     "Content-Type":"application/json"})
-    request.add_header("Authorization", "Bearer "+bearer)
-    contents = urllib2.urlopen(request).read()
-    return contents
-    
+access_token = "NzE3NmMyMmYtYjYxNS00NTBlLWE1ZGYtMmJhYjdiNzhlNDBmZjUwZWRjNWQtODk4_PF84_6121c98f-64c5-48a2-95f7" \
+               "-6e1f074ad37f "
+app = Flask(__name__)
 
-@post('/')
-def index(request):
-    """
-    When messages come in from the webhook, they are processed here.  The message text needs to be retrieved from Spark,
-    using the sendSparkGet() function.  The message text is parsed.  If an expected command is found in the message,
-    further actions are taken. i.e.
-    /batman    - replies to the room with text
-    /batcave   - echoes the incoming text to the room
-    /batsignal - replies to the room with an image
-    """
-    webhook = json.loads(request.body)
-    result = sendSparkGET('https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
-    result = json.loads(result)
-    msg = None
-    if webhook['data']['personEmail'] != bot_email:
-        in_message = result.get('text', '').lower()
-        in_message = in_message.replace(bot_name, '')
-        if 'batman' in in_message or "whoareyou" in in_message:
-            msg = "I'm Batman!"
-        elif 'batcave' in in_message:
-            message = result.get('text').split('batcave')[1].strip(" ")
-            if len(message) > 0:
-                msg = "The Batcave echoes, '{0}'".format(message)
-            else:
-                msg = "The Batcave is silent..."
-        elif 'batsignal' in in_message:
-            print ("NANA NANA NANA NANA")
-            sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "files": bat_signal})
-        if msg != None:
-            print (msg)
-            sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "text": msg})
-    return "true"
 
-####CHANGE THESE VALUES#####
-bot_email = "botreminder@webex.bot"
-bot_name = "BotReminder"
-bearer = "NTU5M2Y1MjktYzQ1ZC00MjhkLTk0MTktYTYxNjc0YTcwYzM3N2E0OTMxZGQtYWE3_PF84_6121c98f-64c5-48a2-95f7-6e1f074ad37f"
-bat_signal  = "https://upload.wikimedia.org/wikipedia/en/c/c6/Bat-signal_1989_film.jpg"
-port1 = int(os.environ.get("PORT", 3000))
-run_itty(server='wsgiref', host='0.0.0.0', port=port1)
+def name(person_id):
+    headers = {'Authorization': 'Bearer ' + access_token, 'content-type': 'application/json'}
+    response = requests.get("https://api.ciscospark.com/v1/people/" + person_id, headers=headers)
+    response = json.loads(response.text)
+    return response["displayName"]
+
+
+def post_message(person_id, room_id):
+    headers = {'Authorization': 'Bearer ' + access_token, 'content-type': 'application/json'}
+    data = {'roomId': room_id, 'text': 'Hello ' + name(person_id) + ', How are you doing?'}
+    response = requests.post("https://api.ciscospark.com/v1/messages", json=data, headers=headers)
+    return response.text
+
+
+@app.route('/', methods=['POST'])
+def main():
+    json_data = request.json
+    data = json_data['data']
+    person_email = data['personEmail']
+    if person_email == "testbot2.1@webex.bot":
+        return "DONE"
+    person_id = data["personId"]
+    room_id = data["roomId"]
+    return json.loads(post_message(person_id, room_id))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
